6e48ce442672d165fc1bb8c6df4d270ca117003c
Sajib Kumar Mitra <niloykesslar@gmail.com>
2023-03-20 23:22:53 +1100

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,17 @@ var express = require("express")
 var app = express()
 var port = process.env.port || 3000;
 
+function addNumbers(number1, number2){
+    return number1 + number2;
+}
+
+app.get('/addTwoNumbers', (req, res) =>{
+    var number1= req.query.number1;
+    var number2= req.query.number2;
+    var result= addNumbers(number1, number2);
+    res.jason({statusCode:200, data:result, messgae:'Success'});
+});
+
 app.listen(port,()=>{
     console.log("App listening to: "+port)
 })
